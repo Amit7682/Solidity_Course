6e93ab07cc3d1fc5192f8e524c4bde69a4bea8f7
Amit7682 <93670022+Amit7682@users.noreply.github.com>
2023-03-11 20:32:37 +0530

    Add files via upload

---
+++ lecture_03_local.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract Local{
+
+      string name = " Amit";
+
+    function local() pure public returns(uint){
+        uint age = 20;
+        return age;
+    }
+}
